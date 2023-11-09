4fec17a9add7024cda6b6c7d2272689e380689e9
lgwk42 <luisgunkim@gmail.com>
2023-11-09 15:43:47 +0900

    [Feature] :: 서버 기본 세팅

---
+++ src/main/java/com/project/oneMind/OneMindApplication.java
@@ -0,0 +1,13 @@
+package com.project.oneMind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneMindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneMindApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/oneMind/OneMindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.oneMind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneMindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
